d4d6e56e7b09f3d176fa8abf0d67e9d3d833d1b0
Steven Blake <s.blake.08@aberdeen.ac.uk>
2016-03-05 10:07:25 +0000

    Code formatting.

--- recaptcha.go
+++ recaptcha.go
@@ -1,11 +1,11 @@
 package recaptcha
 
 import (
-  "net/http"
-  "net/url"
-  "io/ioutil"
-  "encoding/json"
-  "time"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
 )
 
 var postUrl = "https://www.google.com/recaptcha/api/siteverify"
@@ -13,63 +13,63 @@ var postUrl = "https://www.google.com/recaptcha/api/siteverify"
 // Recaptcha struct stores the recaptcha configuration as well as the errors received
 // from the Verify function.
 type Recaptcha struct {
-  secret string
-  timeout int
-  errors []string
+	secret  string
+	timeout int
+	errors  []string
 }
 
 type verifyResponse struct {
-  Success bool
-  ErrorCodes []string `json:"error-codes"`
+	Success    bool
+	ErrorCodes []string `json:"error-codes"`
 }
 
 // Timeout sets the timeout (in seconds) to use when verifying the recaptcha.
 func Timeout(timeout int) func(*Recaptcha) {
-  return func(r *Recaptcha) {
-    r.timeout = timeout
-  }
+	return func(r *Recaptcha) {
+		r.timeout = timeout
+	}
 }
 
 // New returns a new Recaptcha struct using specified secret and any additional options.
 // Default timeout is 30 seconds.
 func New(secret string, options ...func(*Recaptcha)) *Recaptcha {
-  r := &Recaptcha{secret: secret, timeout: 30}
+	r := &Recaptcha{secret: secret, timeout: 30}
 
-  for _, option := range options {
-    option(r)
-  }
+	for _, option := range options {
+		option(r)
+	}
 
-  return r
+	return r
 }
 
 // Verify the recaptcha response, will return true or false. Any errors received will be
 // stored in recaptcha struct.
 func (recaptcha *Recaptcha) Verify(ipAddress string, response string) (bool, error) {
-  recaptcha.errors = nil
-  client := http.Client{Timeout: 30 * time.Second}
+	recaptcha.errors = nil
+	client := http.Client{Timeout: 30 * time.Second}
 
-  resp, err := client.PostForm(postUrl, url.Values{"secret": {recaptcha.secret}, "response": {response}, "remoteip": {ipAddress}})
-  if err != nil {
-    return false, err
-  }
-  defer resp.Body.Close()
+	resp, err := client.PostForm(postUrl, url.Values{"secret": {recaptcha.secret}, "response": {response}, "remoteip": {ipAddress}})
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
 
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return false, err
-  }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
 
-  vr := new(verifyResponse)
-  if err := json.Unmarshal(body, &vr); err != nil {
-    return false, err
-  }
+	vr := new(verifyResponse)
+	if err := json.Unmarshal(body, &vr); err != nil {
+		return false, err
+	}
 
-  recaptcha.errors = vr.ErrorCodes
+	recaptcha.errors = vr.ErrorCodes
 
-  return vr.Success, nil
+	return vr.Success, nil
 }
 
 // GetErrors returns the error that occurred during last recaptcha attempt.
 func (recaptcha *Recaptcha) GetErrors() []string {
-  return recaptcha.errors
+	return recaptcha.errors
 }
--- recaptcha_test.go
+++ recaptcha_test.go
@@ -1,81 +1,81 @@
 package recaptcha
 
 import (
-  "testing"
-  "reflect"
-  "net/http/httptest"
-  "net/http"
-  "encoding/json"
-  )
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
 
 func TestSecretGetsSet(t *testing.T) {
-  secretKey := "My Secret Key"
+	secretKey := "My Secret Key"
 
-  r := New(secretKey)
+	r := New(secretKey)
 
-  if r.secret != secretKey {
-    t.Error("Expected %s, got %s", secretKey, r.secret)
-  }
+	if r.secret != secretKey {
+		t.Error("Expected %s, got %s", secretKey, r.secret)
+	}
 }
 
 func TestDefaultTimeoutSetTo30(t *testing.T) {
-  r := New("")
+	r := New("")
 
-  if r.timeout != 30 {
-    t.Error("Expected 30, got %d", r.timeout)
-  }
+	if r.timeout != 30 {
+		t.Error("Expected 30, got %d", r.timeout)
+	}
 }
 
 func TestTimeoutCanBeSetManually(t *testing.T) {
-  timeout := 80
+	timeout := 80
 
-  r := New("", Timeout(timeout))
+	r := New("", Timeout(timeout))
 
-  if r.timeout != timeout {
-    t.Error("Expected %d, got %d", timeout, r.timeout)
-  }
+	if r.timeout != timeout {
+		t.Error("Expected %d, got %d", timeout, r.timeout)
+	}
 }
 
 func TestGetErrorsReturnsNilWhenNoErrors(t *testing.T) {
-  r := New("")
+	r := New("")
 
-  if r.GetErrors() != nil {
-    t.Error("Expected nil, got ", r.GetErrors())
-  }
+	if r.GetErrors() != nil {
+		t.Error("Expected nil, got ", r.GetErrors())
+	}
 }
 
 func TestVerifySetsSuccess(t *testing.T) {
-  server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/javascript")
-    json.NewEncoder(w).Encode(verifyResponse{
-      Success:true,
-    })
-  }))
-  defer server.Close()
-  postUrl = server.URL
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		json.NewEncoder(w).Encode(verifyResponse{
+			Success: true,
+		})
+	}))
+	defer server.Close()
+	postUrl = server.URL
 
-  r := New("")
-  if success, _ := r.Verify("", ""); success != true {
-    t.Error("Expected true, got false")
-  }
+	r := New("")
+	if success, _ := r.Verify("", ""); success != true {
+		t.Error("Expected true, got false")
+	}
 }
 
 func TestVerifySetsErrorCodes(t *testing.T) {
-  errorCodes := []string{"Error1", "Error2"}
-  server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/javascript")
-    json.NewEncoder(w).Encode(verifyResponse{
-      Success: false,
-      ErrorCodes: errorCodes,
-    })
-  }))
-  defer server.Close()
-  postUrl = server.URL
+	errorCodes := []string{"Error1", "Error2"}
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		json.NewEncoder(w).Encode(verifyResponse{
+			Success:    false,
+			ErrorCodes: errorCodes,
+		})
+	}))
+	defer server.Close()
+	postUrl = server.URL
 
-  r := New("")
-  r.Verify("", "")
+	r := New("")
+	r.Verify("", "")
 
-  if !reflect.DeepEqual(r.GetErrors(), errorCodes) {
-    t.Error("Expected ", errorCodes, " got ", r.GetErrors())
-  }
+	if !reflect.DeepEqual(r.GetErrors(), errorCodes) {
+		t.Error("Expected ", errorCodes, " got ", r.GetErrors())
+	}
 }
